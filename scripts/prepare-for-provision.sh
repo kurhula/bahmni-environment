115f978865a099c76013ca4e9f58de71107a88d1
SwathiVarkala <swathivarkala@gmail.com>
2015-04-01 10:49:47 +0530

    prepare provision

--- scripts/prepare-for-provision.sh
+++ scripts/prepare-for-provision.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 # Ideally everything should be installed via puppet but for puppet to run certain utilities need to be installed for puppet to run
-sudo yum install ruby
+sudo yum -y install ruby
 sudo rpm -ivh http://yum.puppetlabs.com/el/6/products/x86_64/puppetlabs-release-6-7.noarch.rpm
-sudo yum install puppet
-echo 'export IMPLEMENTATION_NAME=$1' >> ~/.bashrc
-mkdir /packages
-mkdir /packages/tools
-mkdir /packages/servers
-wget -r -nH --no-parent --reject="index.html*" https://bahmni-repo.twhosted.com/tools/ -P /packages
-wget -r -nH --no-parent --reject="index.html*" https://bahmni-repo.twhosted.com/servers/ -P /packages
+sudo yum -y install puppet
+
+sudo mkdir /packages/{localrepo,servers,tools,python-packages}
+
+sudo wget -r -nH --no-parent --reject="index.html*" https://bahmni-repo.twhosted.com/localrepo/ -P /packages
+sudo wget -r -nH --no-parent --reject="index.html*" https://bahmni-repo.twhosted.com/tools/ -P /packages
+sudo wget -r -nH --no-parent --reject="index.html*" https://bahmni-repo.twhosted.com/servers/ -P /packages
+sudo wget -r -nH --no-parent --reject="index.html*" https://bahmni-repo.twhosted.com/python-packages/ -P /packages
+sudo chmod -R 777 /packages
+
+sudo sed -i '/^127\.0\.0\.1/ s/$/'" $HOSTNAME"'/' /etc/hosts
